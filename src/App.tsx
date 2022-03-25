1c131f74f2f3d58a9dbe49a3c83bdf879e7157c6
HayatoNabetani <nabe_baseball1111@yahoo.co.jp>
2022-03-25 11:19:24 +0900

    material-uiの導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './App.css';
+import Button from "@mui/material/Button";
+import { Typography } from '@mui/material';
+import HomeIcon from "@mui/icons-material/Home";
 
 const App = () => {
   return (
